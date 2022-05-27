75edb49671d6298080c20763398ebe523b9ef978
EETagent <vojta.jungmann@gmail.com>
2022-05-27 12:49:00 +0200

    fix: přidání 2021-rok, oprava na iPhone SE

--- src/solidJS/components/Copyright.tsx
+++ src/solidJS/components/Copyright.tsx
@@ -4,17 +4,19 @@ import githubIcon from "../../assets/images/github.svg";
 
 const Copyright: Component = () => {
   return (
-    <div class="w-full flex py-6 text-center bg-gray-200 items-center justify-center">
-      <span class="mr-2">© {new Date().getFullYear()}</span>
+    <div class="w-full flex flex-col sm:flex-row py-6 text-center bg-gray-200 items-center justify-center">
+      <div class="flex">
+        <span class="mr-2">© 2021-{new Date().getFullYear()}</span>
+        <a
+          class="underline hover:text-blue-800"
+          href="mailto:&#118;&#111;&#106;&#116;&#097;&#046;&#106;&#117;&#110;&#103;&#109;&#097;&#110;&#110;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;"
+        >
+          Vojtěch Jungmann
+        </a>
+      </div>
+      <span class="hidden sm:block mx-2">|</span>
       <a
-        class="underline hover:text-blue-800"
-        href="mailto:&#118;&#111;&#106;&#116;&#097;&#046;&#106;&#117;&#110;&#103;&#109;&#097;&#110;&#110;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;"
-      >
-        Vojtěch Jungmann
-      </a>
-      <span class="mx-2">|</span>
-      <a
-        class="flex items-center underline"
+        class="mt-2 sm:mt-0 flex items-center underline"
         target="_blank"
         href="https://github.com/EETagent/Kyberfestival"
       >
